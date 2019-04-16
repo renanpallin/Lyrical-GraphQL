a97a2dc1219325089bacb549a71ab76787402330
Renan Pallin <renanpallin@gmail.com>
2019-04-16 15:14:07 -0300

    SongCreate form

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -4,14 +4,21 @@ export default class SongCreate extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.state = { title: '' };
 	}
 
 	render() {
 		return (
 			<div>
 				<h3>Create a new song</h3>
-				<form action="" />
+				<form>
+					<label>Song Title:</label>
+					<input
+						type="text"
+						onChange={e => this.setState({ title: e.target.value })}
+						value={this.state.title}
+					/>
+				</form>
 			</div>
 		);
 	}
